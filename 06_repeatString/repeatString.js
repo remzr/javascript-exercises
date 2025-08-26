c0a2060bebe4a642144be7156f7dc572e8ab4a69
remzr <110097561+remzr@users.noreply.github.com>
2025-08-26 22:12:41 +0200

    Solve sixth JS exercise

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
 
+
+const repeatString = function(string, num) {
+    let stringReturn ="";
+    
+    if (num >= 0) {
+        for (let i = 0; i < num; i++) {     
+        stringReturn = stringReturn + string;
+    }
+    return stringReturn;
+    
+    }   else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
