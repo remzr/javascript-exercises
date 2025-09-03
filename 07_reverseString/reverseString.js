ddd8f3920b5909027a3514cab74d71531da9d420
remzr <110097561+remzr@users.noreply.github.com>
2025-09-03 15:13:55 +0200

    Solve seventh exercise

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    const reversedStr = str.split("").reverse().join("");
+    return reversedStr;
 };
 
 // Do not edit below this line
