0a611cbe9a9cce8e26e64b7efebb473fc94fc89c
remzr <110097561+remzr@users.noreply.github.com>
2025-09-03 22:32:44 +0200

    Solve Exercise 8, with a lot of help.

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(arr) {
+    let filteredArray = [];
+    
+    for (let item of arr) {
+        if ((item == 1 || item == 2 || item == 4) && (!filteredArray.includes(item))) {
+            filteredArray.push(item);
+        }
+    }
+    return filteredArray;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
