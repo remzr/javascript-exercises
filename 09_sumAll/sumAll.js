3d52088f0027854dc055c541967c1c8959e0e6c4
remzr <110097561+remzr@users.noreply.github.com>
2025-09-04 21:05:13 +0200

    Solve exercise Nr 9. Great fun.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    
+    let totalSum = 0;
+    let numFirst = 0;
+    let numSecond = 0;
 
+    if (a < 0 || b < 0 || !Number.isInteger(a + b)) {
+        return "ERROR";
+    } else if (a < b) {
+        numFirst = a, numSecond = b;
+    } else {
+        numFirst = b, numSecond = a;
+    }
+    
+    for (let i = numFirst; i <= numSecond; i++) {
+         totalSum = totalSum + numFirst;
+         numFirst++;
+    }
+    return totalSum;
 };
 
 // Do not edit below this line
