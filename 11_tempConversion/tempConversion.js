3279b2e7a66fc50ce36e3b65cca15bd4f60abe2c
remzr <110097561+remzr@users.noreply.github.com>
2025-09-04 21:51:10 +0200

    Solve exercise Nr. 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  return Math.round(((num -32) / 1.8)*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  return Math.round(((num * 1.8) + 32)*10)/10;
 };
 
 // Do not edit below this line
